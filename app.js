94071bd04a723f5c26413455257295b84bb41c12
Zack Nguyen <thean781@gmail.com>
2023-03-09 12:15:44 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function showPrivate() {  
+    // Write your own code!
+}
+
+function openURL() {
+    // Write your own code!
+}
+
+window.onload = function () {
+    let unveil = document.getElementById('unveil');
+    let contact = document.getElementById('contact');
+
+    // Write your own code!
+}
---
+++ profile.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="main.css">
+  <script type="text/javascript" src="app.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <div class="container">
+    <div class="left card">
+        <!-- Write your own code! -->
+        <img src="./image/free-portrait.jpg"/>
+        <h1>Zack Nguyen</h1>
+        <p class="school">KAIST School of Computer</p>
+         <!-- Change with your own link! -->
+        <p>My favorite Youtube video</p>
+        <a href="https://www.youtube.com/watch?v=N8xEgSe5RwE">Data Security: Protect your critical data </a>
+        <p><button id="contact">Contact</button></p>
+    </div>
+    <div class="right card">
+      <!-- Write your own code! --> 
+      <div id="private" style="">
+        <h2>Private Info</h1>
+        <!-- Write your own code! -->
+        <h3>Introduction</h3>
+        <p>My name is Zack</p>
+        <h3>Home Address</h3>
+        <p>291 Daeharok, Yuseong-gu, Daejon, Korea/p>
+        <h3>Phone Number</h3>
+        <p>123456789</p>
+        <h3>Portal Info</h3>
+        <p>admin / password</p>
+        <a href="#" class="fa fa-linkedin"></a>
+      </div>
+      <p><button id="unveil">Unveil</button></p>
+    </div>
+  </div>
+</body>
+</html>
+
+
+
+
